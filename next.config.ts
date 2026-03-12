7ade68f67d9d8eb04a45d2aa744f09e3e0b913ce
kritam dahal <113635777+Kritam123@users.noreply.github.com>
2026-03-12 15:00:47 +0545

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "dbs-website.ratoguras.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "http",
+        hostname: "dbs-website.ratoguras.com",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
